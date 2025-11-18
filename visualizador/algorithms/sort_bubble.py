3f574817563eaa11d2bb7180881b9991c94a98e9
MatiasCaba07 <matiascaballero227@gmail.com>
2025-11-18 18:35:07 -0300

    Update sort_bubble.py

--- visualizador/algorithms/sort_bubble.py
+++ visualizador/algorithms/sort_bubble.py
@@ -12,29 +12,19 @@ def init(vals):
 
 def step():
     global elementos, norte, i, j
-
-    # Si ya terminamos todas las pasadas
     if i >= norte - 1:
         return {"done": True}
-
-    # Si j llegó al final de la pasada, avanzar i
     if j >= norte - i - 1:
         i += 1
         j = 0
         if i >= norte - 1:
             return {"done": True}
-
-    # Indices
     a = j
     b = j + 1
     swap = False
-
-    # Swap si corresponde
     if elementos[a] > elementos[b]:
         elementos[a], elementos[b] = elementos[b], elementos[a]
         swap = True
-
-    # Avanzar j
     j += 1
 
     return {
