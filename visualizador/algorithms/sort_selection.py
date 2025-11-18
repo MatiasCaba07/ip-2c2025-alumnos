e8febc6988399b058523eeda187b54d4aca1c180
MatiasCaba07 <matiascaballero227@gmail.com>
2025-11-18 18:36:16 -0300

    Update sort_selection.py

--- visualizador/algorithms/sort_selection.py
+++ visualizador/algorithms/sort_selection.py
@@ -15,16 +15,46 @@ def init(vals):
     j = i + 1
     min_idx = i
     fase = "buscar"
-
-def step():
-    global items, n, i, j, min_idx, fase #lo hice yo aaaa
-    if fase == "buscar": # ete tambien
     
-    # TODO:    
-    # - Fase "buscar": comparar j con min_idx, actualizar min_idx, avanzar j.
-    #   Devolver {"a": min_idx, "b": j_actual, "swap": False, "done": False}.
-    #   Al terminar el barrido, pasar a fase "swap".
-    # - Fase "swap": si min_idx != i, hacer ese único swap y devolverlo.
-    #   Luego avanzar i, reiniciar j=i+1 y min_idx=i, volver a "buscar".
-    # Cuando i llegue al final, devolvé {"done": True}.
-    return {"done": True}
+def step():
+    global items, n, i, j, min_idx, fase
+
+    if i >= n - 1:
+        a = -1
+        b = -1
+        swap = False
+        terminado = True
+        return (a, b, swap, terminado)
+
+    if fase == "buscar":
+        a = min_idx
+        b = j
+        swap = False
+        terminado = False
+
+        if j < n:
+            if items[j] < items[min_idx]:
+                min_idx = j
+
+            j += 1
+            return (a, b, swap, terminado)
+
+        fase = "swap"
+
+    if fase == "swap":
+        swap = False
+        if min_idx != i:
+            items[i], items[min_idx] = items[min_idx], items[i]
+            swap = True
+        a = i
+        b = min_idx
+        terminado = False
+        i += 1
+        j = i + 1
+        min_idx = i
+        fase = "buscar"
+
+        if i >= n - 1:
+            terminado = True
+
+        return (a, b, swap, terminado)
